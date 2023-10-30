d14f5cc4bb147e5e6e0092a352faf314f20a26c5
Michał Stryjski <mstryjek88@wp.pl>
2023-10-30 15:41:55 +0100

    Add styles for skills section on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
       },
       maxWidth:{
         'ms':'82rem'
+      },
+      borderRadius:{
+        'wtf':'0.625rem'
+      },
+      boxShadow:{
+        'innerLight':'inset 0 2px 0 0 rgba(255,255,255,0.15)'
       }
     },
   },
