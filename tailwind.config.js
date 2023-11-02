d69ce60b76d96d5f50f65f5df702309760820b5e
Michał Stryjski <mstryjek88@wp.pl>
2023-11-02 13:18:11 +0100

    Add desktop styles for about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,16 @@ module.exports = {
       fontFamily: {
         'dmsans': ['DM Sans', 'sans-serif'],
       },
-      maxWidth:{
-        'ms':'82rem'
+      maxWidth: {
+        'ms': '82rem'
       },
-      borderRadius:{
-        'wtf':'0.625rem'
+      borderRadius: {
+        'mobile': '0.625rem',
+        'desktop': '1.250rem'
       },
-      boxShadow:{
-        'innerLight':'inset 0 2px 0 0 rgba(255,255,255,0.15)',
-        'innerLightmini':'inset 0 1px 0 0 rgba(255,255,255,0.15)'
+      boxShadow: {
+        'innerLight': 'inset 0 2px 0 0 rgba(255,255,255,0.15)',
+        'innerLightmini': 'inset 0 1px 0 0 rgba(255,255,255,0.15)'
       }
     },
   },
